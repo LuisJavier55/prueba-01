f170efe9341cac156c6e4e042fa309a5c89d5990
LuisJavier55 <112447355+LuisJavier55@users.noreply.github.com>
2024-10-23 21:16:40 -0600

    solucion de tarea, agregar funcion en boton la cual envia un parametro=valor y consumirlo en la URL

--- src/FetchWithInput.jsx
+++ src/FetchWithInput.jsx
@@ -6,13 +6,13 @@ import { useFetchAndInput } from "./hooks/useFetchAndInput"
 export const FetchWithInput = () => {
 
   //Tarea completar el Input que si hay un numero integre el valor en la URL 
-  
-
-
-   
+ 
    // eslint-disable-next-line no-unused-vars
-   const {valor, onInputCange,data,loading,hasError,error,onBoton} = useFetchAndInput(0,`https://pokeapi.co/api/v2/pokemon/2`)
+   const {valor, onInputCange,data,loading,hasError,error,obtenerDato} = useFetchAndInput(1)// customHook de nuestros parametros y funciones, enviando nuestro valorInicial 
    
+   const onBoton = ()=>{// funcion boton al precionar 
+     obtenerDato(valor);// cuando se preciona el boton se envia de nuevo el 'valor' a la funcion ObtenerDato como parametro 
+    }
 
     
   return (
@@ -30,16 +30,19 @@ export const FetchWithInput = () => {
         onChange={onInputCange}/>
 
     <p>{valor}</p>
-
-    <button className="btn btn-primary mt-3" onClick={onBoton()}>buscar</button>
+{/* boton donde el evento OnCliCK tiende a llamar la funcion y envia nuestro valor al renderizar */}
+    <button className="btn btn-primary mt-3" onClick={onBoton}>buscar</button> 
     
     
     {
+      //funcion cortocircuito si loading es verdadero envia un mensaje de cargar, sino lo es te manda un error por el consumo de nustra API
     loading
     ?
     <p className="text-primary mt-2 text-center display-1">Cargando...</p>
     :
+    // mensajes llegando desde nuestro CustomHook objeto error tiene estatus?
     <p className="text-primary mt-2 text-center display-1">{error?.estatus} {error?.mensaje}</p>
+    //                                                                       objeto error tiene mensaje ? verdad:mesaje  falso:undefined
     }
     
     <h2 className="text-primary mt-2 text-center display-1">{data?.name}</h2>
--- src/hooks/useFetchAndInput.js
+++ src/hooks/useFetchAndInput.js
@@ -1,29 +1,31 @@
 import { useEffect, useState } from "react"
 
 
-export const useFetchAndInput = (valorInicial,url) => {
+export const useFetchAndInput = (valorInicial) => { //Creamos nuestro CustomHook => useFetchAndInput, pasandole el valor del input 
 
-const [valor, setValor] = useState(valorInicial);
-const [estado, setEstado] = useState({
+const [valor, setValor] = useState(valorInicial);// valorInicial del Input entra a nuestro useState
+const [estado, setEstado] = useState({// Mandamos un objeto donde almacenaremos los datos de la API 
     data:null,
     loading:true,
     hasError:false,
     error: null
 });
 
-useEffect(()=>{
-obtenerDato();
-},[url])
+useEffect(()=>{//UseEffect  se ejecutara solo una vez al renderizar 
+obtenerDato(valor);// funcion la cual obtendra el valor en el parametro el valor Inicial ya que sino causa error al no otorgarle un parametro valor inicial
+},[])
 
-    const obtenerDato = async() =>{
-      const resp = await fetch(url);
 
-      if(!resp.ok){
+    const obtenerDato = async(valorInput) =>{ // funcion asincrona donde tendra la API 
+      const url = `https://pokeapi.co/api/v2/pokemon/${valorInput}`;
+      const resp = await fetch(url); // Espera la respuesta de la url
+
+      if(!resp.ok){//si el estado no es correcto 200 manda objeto 
         setEstado({
             data:null,
             loading:false,
             hasError:true,
-            error: {
+            error: {// mensaje de error 
               estatus:resp.status,
               mensaje:'Error al cargar'
             }
@@ -31,7 +33,7 @@ obtenerDato();
         return;
       }
 
-      const info = await resp.json();
+      const info = await resp.json(); // espera la respuesta convertida en json 
 
       setEstado({ //importante siempre devemos de devolver el estado cambiado y otorgandole los datos si se ejecuta bien
         data:info,
@@ -44,26 +46,20 @@ obtenerDato();
     }
 
 
-    const onInputCange = ({target}) =>{
+    const onInputCange = ({target}) =>{ // funcion que desestructura el evento => para que el valor del input se integre 
         
-         const {value, valueAsNumber} = target;
+         const {value, valueAsNumber} = target; //desetructuracion  
             console.log({value, valueAsNumber});
 
-        setValor(valueAsNumber)
+        setValor(value) // useState renderiza el valor otorgado por el Input 
         console.log(valor);
     }
 
-    const onBoton = () =>{
-      if(!valor === ''){
-        setValor(valor)
-      }
 
-    }
-
-  return {
+  return { //enviamos nuestras funciones y parametros del objeto para consumir nustro CustomHook
     valor,
     onInputCange,
-    onBoton,
+    obtenerDato,
     data:estado.data,
     loading:estado.loading,
     hasError:estado.hasError,
